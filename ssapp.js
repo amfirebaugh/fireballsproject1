836cb63f043e6b6028d265b3828d9e9de586cf33
Phoenicis <alliefirebaugh@gmail.com>
2018-12-29 11:32:44 -0500

    updating app branch without html file

--- ssapp.js
+++ ssapp.js
@@ -1,1 +1,13 @@
 console.log("Hello world");
+
+// NASA Images
+var title = "mars"
+var queryURL1 = "images-api.nasa.gov/search?q=" + title;
+
+$.ajax({
+    url: queryURL1,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    console.log(response.links.href);
+  });
