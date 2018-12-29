d2d8a9ff8011acdb0515379fda700b47b4a79f85
Phoenicis <alliefirebaugh@gmail.com>
2018-12-29 15:05:18 -0500

    updated background image from NASA APOD API

--- ssapp.js
+++ ssapp.js
@@ -1,17 +1,3 @@
-// NASA Images
-// var title = "mars"
-// var queryURL1 = "images-api.nasa.gov/search?q=" + title;
-
-// $.ajax({
-//     url: queryURL1,
-//     method: "GET"
-//   }).then(function(response) {
-//     console.log(response);
-//     console.log(response.links.href);
-//   });
-
-// https://api.nasa.gov/planetary/apod?api_key=4PvAo6XRKPmQI7X7QAYEYvAeYYRERXf8DV4eqGiH
-
 var queryURL2 = "https://api.nasa.gov/planetary/apod?api_key=4PvAo6XRKPmQI7X7QAYEYvAeYYRERXf8DV4eqGiH";
 
 $.ajax({
